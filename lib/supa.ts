bfe76ca68b818e74f871aa41dd3d7532cbf999d2
Matias <matiasdonato14@gmail.com>
2022-11-24 16:45:56 -0300

    .

--- lib/supa.ts
+++ lib/supa.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const sex = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
-const supabase = createClient(sex, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
