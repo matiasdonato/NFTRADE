2fc890ed92fc296e2b005c4da7782adcb3504846
Matias <matiasdonato14@gmail.com>
2022-11-24 18:59:54 -0300

    .

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,3 +1,6 @@
+import Cry from '@assets/cry.webp'
+import BarChart from '@components/charts/barchart'
+import PieChart from '@components/charts/pieChart'
 import NavBar from '@components/dashboard/Components/navbar'
 import SideBar from '@components/dashboard/Components/sidebar'
 import SvgLoading from '@components/icons/svgLoading'
@@ -5,6 +8,8 @@ import fetcher from '@lib/fetcher'
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import useSWR from 'swr'
 import styles from '../../styles/wavinghand.module.css'
 
@@ -59,7 +64,7 @@ const DashBoard: NextPage = () => {
               <p>My coins</p>
             </div>
           </div>
-          {/* <div className="w-full flex items-center flex-wrap justify-between mt-10">
+          <div className="w-full flex items-center flex-wrap justify-between mt-10">
             <div className="bg-[#303339] rounded-2xl w-[60%] p-2">
               <BarChart userData={data} />
             </div>
@@ -86,7 +91,7 @@ const DashBoard: NextPage = () => {
               </div>
             )}
           </div>
-          <div className="w-full flex flex-col mt-10">
+          {/* <div className="w-full flex flex-col mt-10">
             <span className="text-center mb-8 text-2xl font-bold text-gray-400">
               Your <span className="text-blue-600">NFT&apos;s</span>
             </span>
