c2de9652e686f5952c1c09f5f4c25d5bd12a0438
Matias <matiasdonato14@gmail.com>
2022-11-24 16:59:30 -0300

    .

--- lib/supa.ts
+++ lib/supa.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://jrgivjodpnydgnfmeelp.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyZ2l2am9kcG55ZGduZm1lZWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgyMTkzMDksImV4cCI6MTk4Mzc5NTMwOX0.e0l1zWX1U5CE4e6wPDD8jeCF7AO0hmXRQbhrm44teXI'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
