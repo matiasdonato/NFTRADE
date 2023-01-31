8d27345025cd7c443742ec64c4ac4efaeccb6c2c
Matias <matiasdonato14@gmail.com>
2023-01-31 04:10:26 -0300

    fix buy

--- pages/api/collections/buy.ts
+++ pages/api/collections/buy.ts
@@ -11,131 +11,129 @@ export default async function buyCollection(
 ) {
   const { collection, nfts, comprador } = req.body
   try {
-    if (req.method === 'PUT') {
-      const comp = await prisma.user.findUnique({
-        where: {
-          id: comprador.id as string,
-        },
-        select: {
-          id: true,
-          coins: true,
-          name: true,
-        },
-      })
+    const comp = await prisma.user.findUnique({
+      where: {
+        id: comprador.id as string,
+      },
+      select: {
+        id: true,
+        coins: true,
+        name: true,
+      },
+    })
 
-      await prisma?.collection.update({
-        where: {
-          id: collection.id as string,
-        },
-        data: {
-          owner: { connect: { id: comprador.id } },
-          published: false,
-        },
-      })
+    await prisma?.collection.update({
+      where: {
+        id: collection.id as string,
+      },
+      data: {
+        owner: { connect: { id: comprador.id } },
+        published: false,
+      },
+    })
 
-      nfts.forEach(async (el) => {
-        try {
-          await prisma.nft.update({
-            where: {
-              id: el.id as string,
-            },
-            data: {
-              owner: { connect: { id: comprador.id } },
-              published: false,
-            },
-          })
-        } catch (error) {
-          console.log(error)
-          return res.status(401)
-        }
-      })
+    nfts.forEach(async (el) => {
+      try {
+        await prisma.nft.update({
+          where: {
+            id: el.id as string,
+          },
+          data: {
+            owner: { connect: { id: comprador.id } },
+            published: false,
+          },
+        })
+      } catch (error) {
+        console.log(error)
+        return res.status(401)
+      }
+    })
 
-      const collectionComp = await prisma?.collection.findUniqueOrThrow({
-        where: {
-          id: collection.id,
-        },
-        select: {
-          id: true,
-          name: true,
-          owner: true,
-          ownerId: true,
-        },
-      })
+    const collectionComp = await prisma?.collection.findUniqueOrThrow({
+      where: {
+        id: collection.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        owner: true,
+        ownerId: true,
+      },
+    })
 
-      await prisma.user.updateMany({
-        where: {
-          id: comprador.id,
-        },
-        data: {
-          coins: comp.coins - collection.price,
-        },
-      })
+    await prisma.user.updateMany({
+      where: {
+        id: comprador.id,
+      },
+      data: {
+        coins: comp.coins - collection.price,
+      },
+    })
 
-      const vendedor = await prisma.user.findUnique({
-        where: {
-          id: collection.owner.id as string,
-        },
-        select: {
-          id: true,
-          coins: true,
-          name: true,
-        },
-      })
+    const vendedor = await prisma.user.findUnique({
+      where: {
+        id: collection.owner.id as string,
+      },
+      select: {
+        id: true,
+        coins: true,
+        name: true,
+      },
+    })
 
-      await prisma?.user.updateMany({
-        where: {
-          id: vendedor.id,
-        },
-        data: {
-          coins: vendedor.coins + collection.price,
-        },
-      })
+    await prisma?.user.updateMany({
+      where: {
+        id: vendedor.id,
+      },
+      data: {
+        coins: vendedor.coins + collection.price,
+      },
+    })
 
-      await prisma.notify.create({
-        data: {
-          userId: vendedor.id,
-          typeNotify: 'buyNft',
-          nftId: collection.id,
-          nameNft: collectionComp.name,
-          ownerId: collectionComp.owner.id,
-          owner: collectionComp.owner.name,
-          compradorId: comp.id,
-          coins: collection.price,
-          nameComprador: comp.name,
-          vendedorId: vendedor.id,
-          nameVendedor: vendedor.name,
-        },
-      })
-      const date = await prisma?.notify.findMany({
-        where: {
-          userId: vendedor?.id,
-        },
-        orderBy: {
-          createdAt: 'desc',
-        },
-      })
-      const datebuy = date?.shift()
-      emailCollection(
-        req,
-        res,
-        comprador.id,
-        collection.id,
-        vendedor?.name,
-        collection.price,
-        datebuy?.createdAt,
-        'comprador',
-      )
-      emailCollection(
-        req,
-        res,
-        vendedor.id,
-        collection.id,
-        comprador.id,
-        collection.price,
-        datebuy?.createdAt,
-        'vendedor',
-      )
-    }
+    await prisma.notify.create({
+      data: {
+        userId: vendedor.id,
+        typeNotify: 'buyNft',
+        nftId: collection.id,
+        nameNft: collectionComp.name,
+        ownerId: collectionComp.owner.id,
+        owner: collectionComp.owner.name,
+        compradorId: comp.id,
+        coins: collection.price,
+        nameComprador: comp.name,
+        vendedorId: vendedor.id,
+        nameVendedor: vendedor.name,
+      },
+    })
+    const date = await prisma?.notify.findMany({
+      where: {
+        userId: vendedor?.id,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+    const datebuy = date?.shift()
+    emailCollection(
+      req,
+      res,
+      comprador.id,
+      collection.id,
+      vendedor?.name,
+      collection.price,
+      datebuy?.createdAt,
+      'comprador',
+    )
+    emailCollection(
+      req,
+      res,
+      vendedor.id,
+      collection.id,
+      comprador.id,
+      collection.price,
+      datebuy?.createdAt,
+      'vendedor',
+    )
 
     return res.status(200).json({
       msg: 'collection succesfully purchased.',
